eb61e43444ee580273765e103c64c3f2bb4a7502
BellaLima <isabela.lima2@fatec.sp.gov.br>
2022-10-25 21:20:23 -0300

    First commit

---
+++ src/Mensagem.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react/dist/pure';
+import React from 'react';
+
+class Mensagem extends React.Component {
+    shoot = (t) => {
+        console.log(t);
+    }
+    
+    render() {
+        return (
+            <button onClick={()=>this.shoot("Olá Mundo")}> Clique</button>
+        );
+    }
+}
+export default Mensagem;
---
+++ src/Titulo2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Titulo2(){
+    return <h2>Abaixo você verá a tabuada do número escolhido.</h2>
+};
+
+export default Titulo2;
